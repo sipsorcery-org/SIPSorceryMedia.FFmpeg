85125f04bf9676b80a565321e2cd4be84ea946d4
ChristopheI <christophe.irles@al-enterprise.com>
2022-02-22 17:07:39 +0100

    Enhance FFmpeConsoleApp used for test: add video file stream, create AsciiFrame class Add bug buck bunny in "media" folder

--- test/FFmpegConsoleApp/Program.cs
+++ test/FFmpegConsoleApp/Program.cs
@@ -10,15 +10,17 @@ namespace FFmpegConsoleApp
 {
     class Program
     {
-        //private const string LIB_PATH = @"/usr/local/Cellar/ffmpeg/4.4.1_5/lib"; // On MacBookPro
-        private const string LIB_PATH = @"C:\ffmpeg-4.4.1-full_build-shared\bin"; // On Windows
+        // /!\ It's MANDATORY to specify a valid path where FFmpeg binaries / libraries ae stored
 
+        private const string LIB_PATH = @"C:\ffmpeg-4.4.1-full_build-shared\bin"; // On Windows
+        //private const string LIB_PATH = @"/usr/local/Cellar/ffmpeg/4.4.1_5/lib"; // On MacBookPro
         //private const string LIB_PATH = @"..\..\..\..\..\lib\x64";
 
+        // A valid to a video file - usefull if you want to test streaming of a video file
+        const String VIDEO_FILE_PATH = "./../../../../../media/big_buck_bunny.mp4";
+        //const String VIDEO_FILE_PATH = @"https://upload.wikimedia.org/wikipedia/commons/3/36/Cosmos_Laundromat_-_First_Cycle_-_Official_Blender_Foundation_release.webm"; // It's alo possible to set a remote file
 
-        static private VideoFrameConverter converter = null;
-        static readonly StringBuilder builder = new StringBuilder();
-        static readonly char[] asciiPixels = " `'.,-~:;<>\"^=+*!?|\\/(){}[]#&$@".ToCharArray();
+        static private AsciiFrame? asciiFrame = null;
 
         static void Main(string[] args)
         {
@@ -32,42 +34,34 @@ namespace FFmpegConsoleApp
             List<Camera> cameras = FFmpegCameraManager.GetCameraDevices();
             List<Monitor> monitors = FFmpegMonitorManager.GetMonitorDevices();
 
-            // If we have both Camera and Monitors, ask user to choose 
             char keyChar = ' ';
-            if ( (cameras?.Count > 0) && (monitors?.Count > 0) )
+            while (true)
             {
-                while (true)
-                {
-                    Console.Clear();
-                    Console.WriteLine("\nDo we want to use Camera or a Monitor ?");
-                    Console.Write("\n [c] - Camera ");
-                    Console.Write("\n [m] - Monitor ");
-                    Console.WriteLine("\n");
-                    Console.Out.Flush();
+                Console.Clear();
+                if (!(cameras?.Count > 0))
+                    Console.WriteLine("\nNo Camera found ...");
+                if (!(monitors?.Count > 0))
+                    Console.WriteLine("\nNo Monitor found ...");
 
-                    var keyConsole = Console.ReadKey();
-                    if ((keyConsole.KeyChar == 'c') || (keyConsole.KeyChar == 'm'))
-                    {
-                        keyChar = keyConsole.KeyChar;
-                        break;
-                    }
+                Console.WriteLine("\nWhat do you want to use ?");
+                if (cameras?.Count > 0)
+                    Console.Write("\n [c] - Camera ");
+                if (monitors?.Count > 0)
+                    Console.Write("\n [m] - Monitor ");
+                Console.Write($"\n [f] - File - Path:[{VIDEO_FILE_PATH}]");
+
+                Console.WriteLine("\n");
+                Console.Out.Flush();
+
+                var keyConsole = Console.ReadKey();
+                if ( ( (keyConsole.KeyChar == 'c') && (cameras?.Count > 0) )
+                    || ( (keyConsole.KeyChar == 'm') && (monitors?.Count > 0)) 
+                    || (keyConsole.KeyChar == 'f'))
+                {
+                    keyChar = keyConsole.KeyChar;
+                    break;
                 }
             }
-            else
-            {
-                if (cameras?.Count > 0)
-                    keyChar = 'c';
-                else if (monitors?.Count > 0)
-                    keyChar = 'm';
-            }
-
-            // Selection is correct ?
-            if (!((keyChar == 'c') || (keyChar == 'm')))
-            {
-                Console.WriteLine("\nYou have no monitor neither a camera ... It means that somehting bad happened in enumeration");
-                return;
-            }
-
             // Do we manage a camera ?
             if (keyChar == 'c')
             {
@@ -102,7 +96,7 @@ namespace FFmpegConsoleApp
 
             }
             // Do we manage a Monitor ?
-            else 
+            else if (keyChar == 'm')
             {
                 int monitorIndex = 0;
                 if (monitors?.Count > 1)
@@ -133,7 +127,15 @@ namespace FFmpegConsoleApp
                 SIPSorceryMedia.FFmpeg.FFmpegScreenSource screenSource = new SIPSorceryMedia.FFmpeg.FFmpegScreenSource(selectedMonitor.Path, selectedMonitor.Rect, 20);
                 videoSource = screenSource as IVideoSource;
             }
+            // Do we manage a File ?
+            else
+            {
+                SIPSorceryMedia.FFmpeg.FFmpegFileSource fileSource = new SIPSorceryMedia.FFmpeg.FFmpegFileSource(VIDEO_FILE_PATH, false, null, true);
+                videoSource = fileSource as IVideoSource;
+            }
 
+            // Create object used to display video in Ascii
+            asciiFrame = new AsciiFrame();
 
             videoSource.RestrictFormats(x => x.Codec == VideoCodec);
             videoSource.SetVideoSourceFormat(videoSource.GetVideoSourceFormats().Find(x => x.Codec == VideoCodec));
@@ -157,44 +159,7 @@ namespace FFmpegConsoleApp
 
         private static void FileSource_OnVideoSourceRawSample(uint durationMilliseconds, RawImage rawImage)
         {
-            if (converter == null 
-                || Console.WindowWidth != converter.SourceWidth 
-                || Console.WindowHeight != converter.SourceWidth)
-            {
-                // We can't just override converter
-                // We have to dispose the previous one and instanciate a new one with the new window size.
-                converter?.Dispose();
-                converter = new VideoFrameConverter(rawImage.Width, rawImage.Height, AVPixelFormat.AV_PIX_FMT_RGB24, Console.WindowWidth, Console.WindowHeight, FFmpeg.AutoGen.AVPixelFormat.AV_PIX_FMT_GRAY8);
-            }
-
-            // Resize the frame to the size of the terminal window, then draw it in ASCII.
-            var frame = converter.Convert(rawImage.Sample);
-            DrawAsciiFrame(frame);
+            asciiFrame.GotRawImage(ref rawImage);
         }
-
-        private static unsafe void DrawAsciiFrame(AVFrame frame)
-        {
-            // We don't call Console.Clear() here because it actually adds stutter.
-            // Go ahead and try this example in Alacritty to see how smooth it is!
-            builder.Clear();
-            Console.SetCursorPosition(0, 0);
-            int length = frame.width * frame.height;
-
-            var RawData = new ReadOnlySpan<byte>(frame.data[0], frame.linesize[0] * frame.height);
-
-            // Since we know that the frame has the exact size of the terminal window,
-            // we have no need to add any newline characters. Thus we can just go through
-            // the entire byte array to build the ASCII converted string.
-            for (int i = 0; i < length; i++)
-            {
-                builder.Append(asciiPixels[RangeMap(RawData[i], 0, 255, 0, asciiPixels.Length - 1)]);
-            }
-
-            Console.Write(builder.ToString());
-            Console.Out.Flush();
-        }
-
-        public static int RangeMap(int x, int in_min, int in_max, int out_min, int out_max)
-        => (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 }
